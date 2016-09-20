c8562aef9fb4486829a4221a4d600cb649624ca0
unknown <arevalo@merlot.irosoft.com>
2016-09-20 14:36:23 -0400

    Config sample

---
+++ configuration.sample.js
@@ -0,0 +1,24 @@
+var configuration = {
+	"app":{
+		"qrCodeDomain": "http://legishub.irosoft.com/document/"
+	},
+	"server": {
+		"port": 80
+	},
+	"db":[
+		{
+			"type": "mongo",
+			"url": "mongodb://localhost:27017/legalhub"
+		}
+	],
+	"providers": {
+		"ah": '"C:/Program Files/Antenna House/AHFormatterV63/AHFCmd.exe"',
+		"prince": '"C:/Program Files (x86)/Prince/engine/bin/prince.exe"',
+		"pdfbox": 'java -jar "bin/PdfBox/pdfbox-app-2.0.2.jar"',
+		"saxon": 'java -jar "bin/Saxon/saxon9pe.jar"'
+	},
+	"formatters":{
+		"pdf": "ah"
+	}
+};
+module.exports = configuration;
